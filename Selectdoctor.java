574f1da319e11833e58bd23cd196b48b01fa82cb
raghavendraivv <ivvraghavendra1999@gmail.com>
2020-05-12 00:27:58 +0530

    Completed Select Doctot

--- Selectdoctor.java
+++ Selectdoctor.java
@@ -5,13 +5,18 @@
  */
 package onlinedoctorappointmentsystem;
 
+import java.util.*;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import static onlinedoctorappointmentsystem.ManageProfile.user1;
 
 /**
@@ -20,19 +25,23 @@ import static onlinedoctorappointmentsystem.ManageProfile.user1;
  */
 public class Selectdoctor extends javax.swing.JFrame {
 
+    public static int id,p=1;
+    public static int doctor_id;
+    public static Date date = new Date();
     /**
      * Creates new form Selectdoctory
      */
     public Selectdoctor() {
         initComponents();
-        int x =0,y=0,w=50;
+        int x ,y,w,p;
+        date=null;
         String docname1 =Specilization.docname;
         String name;
        JLabel[] array = new JLabel[10];
-       JLabel name1 = new JLabel();
-       JLabel time = new JLabel();
-       JLabel gender = new JLabel();
-       JLabel amount = new JLabel();
+       
+       
+      
+       
 //        for(int i=0;i<10;i++)
 //        {
 //            array[i] = new JLabel("raghs"+i);
@@ -53,7 +62,8 @@ public class Selectdoctor extends javax.swing.JFrame {
                 ResultSet rp = stmt.executeQuery("select * from doctor where specailization='"+docname1+"'");
                 x=10;
                 w=110;
-                y=50;
+                y=100;
+               // name1.setText(" " );
                 while(rp.next())
                 {
                     rp.next();
@@ -65,6 +75,7 @@ public class Selectdoctor extends javax.swing.JFrame {
                     name2.setBounds(x,y,50,50);
                     //y+=50
                     
+                    JLabel name1 = new JLabel();
                     name1.setText(String.valueOf(rp.getString(2)));
                     this.add(name1);
                     name1.setOpaque(true);
@@ -78,6 +89,7 @@ public class Selectdoctor extends javax.swing.JFrame {
                     time2.setOpaque(true);
                     time2.setBounds(x,y,50,50);
                     
+                    JLabel time = new JLabel();
                     time.setText(String.valueOf(rp.getString(6)));
                     this.add(time);
                     time.setOpaque(true);
@@ -93,6 +105,7 @@ public class Selectdoctor extends javax.swing.JFrame {
                     gender1.setBounds(x,y,100,50);
                     //y-=50;
                     
+                    JLabel gender = new JLabel();
                     gender.setText(String.valueOf(rp.getString(3)));
                     this.add(gender);
                     gender.setOpaque(true);
@@ -106,12 +119,29 @@ public class Selectdoctor extends javax.swing.JFrame {
                     amount1.setBounds(x,y,100,50);
                    // x+=50;
                     
+                    JLabel amount = new JLabel();
                     amount.setText(String.valueOf(rp.getString(5)));
                     this.add(amount);
                     amount.setOpaque(true);
                     amount.setBounds(w, y, 150, 50);
                   //.setText(String.valueOf(rp.getString(2)));
                   //name1=null;
+                    y+=50;
+                    final int id=rp.getInt(1);
+                    JButton b=new JButton("Book Appointment");  
+                    this.add(b);
+                    b.setOpaque(true);
+                    b.setBounds(20,y,150,30);
+                    b.addActionListener(new ActionListener(){  
+                    public void actionPerformed(ActionEvent e){  
+                    // System.out.println(id);
+                    doctor_id=Selectdoctor.id;
+                    if(Selectdoctor.p==1)
+                    {
+                        JOptionPane.showMessageDialog(rootPane,"Please Select Date!!");
+                    }
+        }  
+    });  
                   x=10;
                   y+=50;
                  // i++;
@@ -133,6 +163,10 @@ public class Selectdoctor extends javax.swing.JFrame {
     private void initComponents() {
 
         jToggleButton1 = new javax.swing.JToggleButton();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jCalendar1 = new com.toedter.calendar.JCalendar();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -143,21 +177,68 @@ public class Selectdoctor extends javax.swing.JFrame {
             }
         });
 
+        jPanel1.setBackground(java.awt.Color.gray);
+
+        jLabel1.setBackground(java.awt.Color.white);
+        jLabel1.setFont(new java.awt.Font("Ubuntu", 3, 48)); // NOI18N
+        jLabel1.setForeground(java.awt.Color.white);
+        jLabel1.setText("Select Doctor");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(276, 276, 276)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(391, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 62, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        jCalendar1.setBackground(java.awt.Color.lightGray);
+
+        jButton1.setText("Confirm Date");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(793, Short.MAX_VALUE)
-                .addComponent(jToggleButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(43, 43, 43))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jToggleButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(137, 137, 137))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jCalendar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(93, 93, 93))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(169, 169, 169))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(28, 28, 28)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(34, 34, 34)
                 .addComponent(jToggleButton1)
-                .addContainerGap(521, Short.MAX_VALUE))
+                .addGap(34, 34, 34)
+                .addComponent(jCalendar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addGap(0, 334, Short.MAX_VALUE))
         );
 
         pack();
@@ -169,6 +250,13 @@ public class Selectdoctor extends javax.swing.JFrame {
         dispose();
         s.setVisible(true);
     }//GEN-LAST:event_jToggleButton1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        p=0;
+        date = jCalendar1.getDate();
+        System.out.println(date);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -206,6 +294,10 @@ public class Selectdoctor extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private com.toedter.calendar.JCalendar jCalendar1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JToggleButton jToggleButton1;
     // End of variables declaration//GEN-END:variables
 }
